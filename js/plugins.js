3cfb0dc53145270a2b72e0fcd49e6a592537480a
abdokjkj <71026403+abdokjkj@users.noreply.github.com>
2020-09-10 14:25:43 +0200

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,12 @@
+
+$(function(){
+
+$(".im p span").on("click",function(){
+
+$(this).css("backgroundColor","orange").siblings().css("background","none")
+$(this).css("padding","5px")
+
+})
+
+
+});
